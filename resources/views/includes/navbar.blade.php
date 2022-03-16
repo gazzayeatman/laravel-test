59b033c56271030ca2940170407200b561482974
Garry Yeatman <garryyeatman@Garrys-MacBook-Pro-2.local>
2022-03-17 12:55:30 +1300

    Styling updates

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -38,22 +38,16 @@
                     Sites
                 </a>
             </li>
-            <li class="navbar__item">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->name }}
+            <li class="navbar__item navbar__item--auth-panel">
+                <a class="navbar__link" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();
+                ">
+                    {{ __('Logout') }}
                 </a>
-
-                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                </div>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
             </li>
         @endguest
     </ul>
